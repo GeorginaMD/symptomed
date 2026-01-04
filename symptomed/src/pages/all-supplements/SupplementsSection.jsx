6369b1cd5cfe4adee7c9c382f916146ce0e1d520
Georgina <georginamircheska@gmail.com>
2026-01-05 00:45:28 +0100

    Update seed data and Schema for image type, fetch supplements from backend and render list and detail page

--- symptomed/src/pages/all-supplements/SupplementsSection.jsx
+++ symptomed/src/pages/all-supplements/SupplementsSection.jsx
@@ -1,14 +1,23 @@
 import SupplementsCard from "./SupplementsCard"
 import "./SupplementsSection.css"
 import ButtonHomePage from "../../components/ButtonHomePage"
+import { useEffect, useState } from "react"
 
 export default function SupplementsSection() {
+
+    const  [supplements, setSupplements] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/supplements")
+        .then(res => res.json())
+        .then(data => setSupplements(data));
+    }, []);
+
     return (
         <section className="supplements-section">
-            <SupplementsCard />
-            <SupplementsCard />
-            <SupplementsCard />
-            <SupplementsCard />
+            {supplements.map(s => (
+                <SupplementsCard key={s.slug} supplement={s}/>
+            ))}
             <br />
             <ButtonHomePage />
         </section>
