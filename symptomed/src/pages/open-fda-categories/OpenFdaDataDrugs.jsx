089979ed89b14f54684a4eca69ca99dbae568160
Georgina <georginamircheska@gmail.com>
2026-01-24 15:07:26 +0100

    Added fallback messages for fetching drugs

--- symptomed/src/pages/open-fda-categories/OpenFdaDataDrugs.jsx
+++ symptomed/src/pages/open-fda-categories/OpenFdaDataDrugs.jsx
@@ -8,11 +8,11 @@ const BASE_URL = "https://api.fda.gov/drug/label.json";
 
 function normalizeDrug(drug) {
     return {
-        name: drug.openfda?.brand_name?.[0] ?? drug.spl_product_data_elements?.[0] ?? drug.package_label_principal_display_panel?.[0] ?? "Unknown",
-        purpose: drug.purpose?.[0] ?? "Not specified",
-        uses: drug.indications_and_usage?.[0] ?? "Not specified",
-        warnings: drug.warnings?.[0] ?? drug.do_not_use?.[0] ?? "No warnings listed",
-        dosage: drug.dosage_and_administration?.[0] ?? "See label",
+        name: drug.openfda?.brand_name?.[0] ?? drug.spl_product_data_elements?.[0] ?? drug.package_label_principal_display_panel?.[0] ?? "Unknown drug name or inexistent in our database.",
+        purpose: drug.purpose?.[0] ?? "The purpose of this drug is not specified.",
+        uses: drug.indications_and_usage?.[0] ?? "The uses of this drugs are not specified.",
+        warnings: drug.warnings?.[0] ?? drug.do_not_use?.[0] ?? "No warnings listed for this drug.",
+        dosage: drug.dosage_and_administration?.[0] ?? "See label or package leaflet.",
     };
 }
 
@@ -47,8 +47,8 @@ export default function OpenFdaDataDrugs() {
 
      console.log(drugs);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>{error}</p>;
+    if (loading) return <p className="loading-drugs">Loading drugs, please wait...</p>;
+    if (error) return <p className="error-drugs">{error}</p>;
 
    
 
