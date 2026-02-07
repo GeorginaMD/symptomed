f9bfba2f6d345a5c9f3d5d30f99d2c9826a3b978
Georgina <georginamircheska@gmail.com>
2026-02-07 13:50:10 +0100

    Added elements and styles to demo login section

--- symptomed/src/pages/user-dashboard-demo-login/DemoLoginProfileSection.jsx
+++ symptomed/src/pages/user-dashboard-demo-login/DemoLoginProfileSection.jsx
@@ -2,13 +2,32 @@ import "./DemoLoginProfileSection.css"
 import DemoUserProfileData from "./DemoUserProfileData"
 import DemoHealthDataDashboard from "./DemoHealthDataDashboard"
 import ButtonHomePage from "../../components/ButtonHomePage"
+import { useContext } from "react"
+import { UserContext } from "../../context/UserContext"
+import { useNavigate } from "react-router"
+
 
 export default function DemoLoginProfileSection() {
+
+    const { logout } = useContext(UserContext);
+    const navigate = useNavigate();
+    const handleDemoLogout = () => {
+        logout();
+        navigate("/demo-login-page", {replace: true});
+    }
+
     return (
-        <section className="demo-login-profile-section">
-            <DemoUserProfileData />
-            <DemoHealthDataDashboard />
-            <ButtonHomePage />
-        </section>
+        <div className="demo-login-profile-div-wrapper">
+            <section className="demo-login-profile-section-wrapper">
+                <div className="demo-login-profile-section">
+                    <DemoHealthDataDashboard />
+                    <DemoUserProfileData />
+                </div>
+            </section>
+            <div className="demo-login-nav-buttons">
+                    <button onClick={handleDemoLogout} className="home-page">Logout</button>
+                    <ButtonHomePage />
+            </div>
+        </div>
     )
 }
