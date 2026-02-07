4fefb8735b47367974f3cf57d5facb1cc2ba7452
Georgina <georginamircheska@gmail.com>
2026-02-07 13:48:23 +0100

    Added media queries for smaller devices

--- symptomed/src/pages/demo-page/DemoLoginForm.jsx
+++ symptomed/src/pages/demo-page/DemoLoginForm.jsx
@@ -4,6 +4,7 @@ import { UserContext } from "../../context/UserContext"
 import { useNavigate } from "react-router"
 import { useContext } from "react"
 import "../../components/ButtonHomePage.css"
+import ButtonHomePage from "../../components/ButtonHomePage"
 
 
 export default function DemoLoginForm() {
@@ -17,25 +18,28 @@ export default function DemoLoginForm() {
     }
 
     return (
-        <div className="demo-form-wrapper">
-            <form className="demo-form" onSubmit={handleDemoLogin}>
-                <p className="demo-p">This demo login lets you explore the app without creating an account.
-                                You’ll be logged in with a predefined demo user to preview the dashboard
-                                and features using example data only!</p>
-                <div className="demo-input-wrapper">
-                    <label htmlFor="demo-email" className="demo-email-label">Email:
-                        <input type="email" id="demo-email" value="demo-login@symptomed.com" className="demo-email" readOnly/>
-                    </label>
-                    <label htmlFor="demo-password" className="demo-password-label">Password:
-                        <input type="password" id="demo-password" value="demo-login2026" className="demo-password" readOnly/>
-                    </label>
-                </div>
-                <button type="submit" className="home-page">
-                    Demo Login
-                </button>
-
-           {/*  <Link to="/user-dashboard-demo-login" className="link"> ? ? ?</Link> */}
-            </form>
+        <div>
+            <div className="demo-form-wrapper">
+                <form className="demo-form" onSubmit={handleDemoLogin}>
+                    <p className="demo-p">This demo login lets you explore the app without creating an account.
+                                    You’ll be logged in with a predefined demo user to preview the dashboard
+                                    and features using example data only!</p>
+                    <div className="demo-input-wrapper">
+                        <label htmlFor="demo-email" className="demo-email-label">Email:
+                            <input type="email" id="demo-email" value="demo-login@symptomed.com" className="demo-email" readOnly title="Demo data cannot be modified"/>
+                        </label>
+                        <label htmlFor="demo-password" className="demo-password-label">Password:
+                            <input type="password" id="demo-password" value="demo-login2026" className="demo-password" readOnly title="Demo data cannot be modified"
+                            />
+                        </label>
+                    </div>
+                    <button type="submit" className="home-page">
+                        Demo Login
+                    </button>
+            {/*  <Link to="/user-dashboard-demo-login" className="link"> ? ? ?</Link> */}
+                </form>
+            </div>
+            <ButtonHomePage />
         </div>
     )
 }
