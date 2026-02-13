3bf0998364c221c3b80e49c66fd86c1371fa5b13
Georgina <georginamircheska@gmail.com>
2026-02-14 00:49:36 +0100

    created post routes for register, login and logout and validation of the body of email and password

---
+++ symptomed-api/routes/authRoutes.js
@@ -0,0 +1,52 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/authController";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Please provide a valid email")
+      .normalizeEmail(),
+
+    body("password")
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long")
+      .matches(/[A-Z]/)
+      .withMessage("Password must contain at least one uppercase letter")
+      .matches(/[a-z]/)
+      .withMessage("Password must contain at least one lowercase letter")
+      .matches(/[0-9]/)
+      .withMessage("Password must contain at least one number")
+      .matches(/[\W_]/)
+      .withMessage("Password must contain at least one special character"),
+  ],
+  registerUser,
+);
+
+router.post(
+  "/login",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Please provide a valid email")
+      .normalizeEmail(),
+
+    body("password".notEmpty().withMessage("Password is required")),
+  ],
+  loginUser,
+);
+
+router.post("/logout", logoutUser);
+
+export default router;
