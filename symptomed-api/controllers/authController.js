0acd0e71c7a41ada39b04c2196d2bf5a50039d9e
Georgina <georginamircheska@gmail.com>
2026-02-14 00:54:20 +0100

    Created register controller for check of existance and creating user, login controller for if user create token and send cookie, and logout controller to clear cookie

---
+++ symptomed-api/controllers/authController.js
@@ -0,0 +1,76 @@
+import User from "../models/User";
+import jwt from "jasonwebtoken";
+import { validationResult } from "express-validator";
+
+//REGISTER controller:
+export const registerUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  try {
+    const { email, password } = req.body;
+    //check if user exists:
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    //create user:
+    const user = await User.create({ email, password });
+    res.status(201).json({ message: "User registered succesfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//LOGIN controller:
+export const loginUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    //if password ^ is correct, create token:
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    //send token as http-only cookie (stored in browser):
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, //true for production
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.json({ message: "Login succesfull" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//LOGOUT controller
+export const logoutUser = (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: false, //true for production
+    sameSite: "strict",
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+};
