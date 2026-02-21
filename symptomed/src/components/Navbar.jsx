df51168f8835b81f6982162d73d1ff850904e627
Georgina <georginamircheska@gmail.com>
2026-02-21 10:54:37 +0100

    Added navigation logic for logout

--- symptomed/src/components/Navbar.jsx
+++ symptomed/src/components/Navbar.jsx
@@ -1,14 +1,30 @@
 import DemoPageView from "./DemoPageView"
 import LoginPageView from "./LoginPageView"
 import RegisterPageView from "./RegisterPageView"
+import { useAuth } from "../context/AuthContext"
+import { useNavigate } from "react-router"
 import "./Navbar.css"
 
 export default function Navbar() {
+    const { user, logout } = useAuth();
+
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        await logout();
+        navigate("/", {replace: true});
+    }
+
     return (
     <div>
         <nav className="Navbar">
             <strong className="h1" title="Health Symptom Explorer">SymptoMed</strong>
             <ul className="nav-ul">
+                {user && (
+                    <>
+                        <span className="p-login-current-user">{user.email}</span>
+                        <button onClick={handleLogout} className="home-page">Logout</button>
+                    </>
+                )}
                 <DemoPageView />
                 <LoginPageView />
                 <RegisterPageView />
@@ -17,3 +33,4 @@ export default function Navbar() {
     </div>
     )
 }
+
