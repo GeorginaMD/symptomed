5dcb68bf65b66716d237d49a4892db2806d81f3f
Georgina <georginamircheska@gmail.com>
2026-02-21 11:11:58 +0100

    Fixed navigation for register

--- symptomed/src/pages/register-page/RegisterForm.jsx
+++ symptomed/src/pages/register-page/RegisterForm.jsx
@@ -1,25 +1,39 @@
 import "./RegisterForm.css"
-import { Link } from "react-router"
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../context/AuthContext";
 
 export default function RegisterForm() {
+    const { register } = useAuth();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const success = await register(email, password);
+        if (success) {
+            navigate("/user-dashboard-login");
+        }
+    };
+
     return (
         <div className="register-form-wrapper">
             
-            <form action="" className="register-form">
+            <form className="register-form" onSubmit={handleSubmit}>
                 <p className="register-p">Please fill your register data:</p>
                 <div className="register-input-wrapper">
                         <label htmlFor="register-email" className="register-email-label">Email:
-                            <input type="email" id="register-email" value="" className="register-email"/>
+                            <input type="email" id="register-email" value={email} className="register-email" onChange={(e) => setEmail(e.target.value)}/>
                         </label>
                         <label htmlFor="register-password" className="register-password-label">Password:
-                            <input type="password" id="register-password" value="" className="register-password"/>
+                            <input type="password" id="register-password" value={password} className="register-password" onChange={(e) => setPassword(e.target.value)}/>
                         </label>
                     </div>
                     <button type="submit" className="home-page">
                         Register
                     </button>
-                <br />
-            <Link to="/user-dashboard-registration" className="link">Submit - Register</Link>
             </form>
         </div>
     )
