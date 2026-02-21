a597b87623d0e50b052f3b8526dd0387148bb14a
Georgina <georginamircheska@gmail.com>
2026-02-21 11:09:37 +0100

    Fixed navigation for login

--- symptomed/src/pages/login-page/LoginForm.jsx
+++ symptomed/src/pages/login-page/LoginForm.jsx
@@ -1,24 +1,38 @@
 import "./LoginForm.css"
-import { Link } from "react-router"
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../context/AuthContext";
 
 export default function LoginForm() {
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const success = await login(email, password);
+        if (success) {
+            navigate("/user-dashboard-login");
+        }
+    };
+
     return (
         <div className="login-form-wrapper">
-            <form action="" className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <p className="login-p">Please fill your login data:</p>
                 <div className="login-input-wrapper">
                     <label htmlFor="login-email" className="login-email-label">Email:
-                        <input type="email" id="login-email" value="" className="login-email"/>
+                        <input type="email" id="login-email" value={email} className="login-email" onChange={(e) => setEmail(e.target.value)}/>
                     </label>
                     <label htmlFor="login-password" className="login-password-label">Password:
-                        <input type="password" id="login-password" value="" className="login-password"/>
+                        <input type="password" id="login-password" value={password} className="login-password" onChange={(e) => setPassword(e.target.value)}/>
                     </label>
                 </div>
                 <button type="submit" className="home-page">
                     Login
                 </button>
-                <br />
-            <Link to="/user-dashboard-login" className="link">Submit - Login</Link>
             </form>
         </div>
     )
