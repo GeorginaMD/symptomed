d71d6f54bbac4d7e26fb694e245e04f560a481f5
Georgina <georginamircheska@gmail.com>
2026-02-21 11:47:26 +0100

    Added link to github profile page

--- symptomed/src/components/DisclaimerModal.jsx
+++ symptomed/src/components/DisclaimerModal.jsx
@@ -4,7 +4,7 @@ export default function DisclaimerModal({ type, onClose }) {
   const content = {
     privacy: `Privacy Notice - 
         We respect your privacy. This application collects and stores only the information necessary to provide its core functionality, such as creating a user account and saving a personal health profile.
-        The information you choose to provide may include general health-related data (such as symptoms, preferences, or supplement interests), but this data is not used for medical diagnosis and is not shared with third parties.Your data is used only to improve your experience within the application. You may request deletion of your account and associated data at any time. We do not sell, rent, or share your personal information with advertisers or external services.`,
+        The information you choose to provide may include general health-related data (such as symptoms, preferences, or supplement interests), but this data is not used for medical diagnosis and is not shared with third parties. Your data is used only to improve your experience within the application. You may request deletion of your account and associated data at any time. We do not sell, rent, or share your personal information with advertisers or external services.`,
     demo: `Demo Mode - 
         This application offers a demo mode with pre-filled example data so that users can explore its functionality without creating an account or providing any personal information.
         The demo data is fictional and does not represent real individuals or medical cases.`,
@@ -12,7 +12,7 @@ export default function DisclaimerModal({ type, onClose }) {
         Some of the information displayed in this application is sourced from publicly available databases, including the U.S. Food and Drug Administration (OpenFDA API).
         This data is provided for informational purposes only and may not be complete, up to date, or applicable to every individual case. The application does not control or modify the original source data.`,
     contact: `Contact - 
-        This application was created and is maintained by [Your Name], a web developer focused on health-related educational tools.For questions, feedback, or data-related requests, you can contact:`
+        This application was created and is maintained by a web developer focused on health-related educational tools. For questions, feedback, or data-related requests, you can contact me: https://github.com/GeorginaMD .`
   };
 
   return (
