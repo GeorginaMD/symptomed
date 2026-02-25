06d18efd16b333f6826c0777884031663826405d
Georgina <georginamircheska@gmail.com>
2026-02-25 14:25:08 +0100

    Added validation for email and password inputs when registering

--- symptomed/src/pages/register-page/RegisterForm.jsx
+++ symptomed/src/pages/register-page/RegisterForm.jsx
@@ -9,9 +9,45 @@ export default function RegisterForm() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    const validateForm = () => {
+        const newErrors = [];
+
+        //for email:
+        if (!email.includes("@")) {
+            newErrors.push("Email must contain '@'");
+        }
+
+        //for password:
+        if (password.length < 8) {
+            newErrors.push("Password must be at least 8 characters long");
+        }
+        if (!/[A-Z]/.test(password)) {
+            newErrors.push("Password must contain at least one uppercase letter");
+        }
+        if (!/[a-z]/.test(password)) {
+            newErrors.push("Password must contain at least one lowercase letter");
+        }
+        if (!/[0-9]/.test(password)) {
+            newErrors.push("Password must contain at least one number");
+        }
+        if (!/[\W_]/.test(password)) {
+            newErrors.push("Password must contain at least one special character");
+        }
+        return newErrors;
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const validationErrors = validateForm();
+        if (validationErrors.length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
+        setErrors([]);
+
         const success = await register(email, password);
         if (success) {
             navigate("/user-dashboard-login");
@@ -20,17 +56,27 @@ export default function RegisterForm() {
 
     return (
         <div className="register-form-wrapper">
-            
             <form className="register-form" onSubmit={handleSubmit}>
                 <p className="register-p">Please fill your register data:</p>
                 <div className="register-input-wrapper">
                         <label htmlFor="register-email" className="register-email-label">Email:
-                            <input type="email" id="register-email" value={email} className="register-email" onChange={(e) => setEmail(e.target.value)}/>
+                            <input type="email" id="register-email" value={email} className="register-email" onChange={(e) => {setEmail(e.target.value); setErrors([]); }} />
                         </label>
                         <label htmlFor="register-password" className="register-password-label">Password:
-                            <input type="password" id="register-password" value={password} className="register-password" onChange={(e) => setPassword(e.target.value)}/>
+                            <input type="password" id="register-password" value={password} className="register-password" onChange={(e) => { setPassword(e.target.value); setErrors([]); }}/>
                         </label>
                     </div>
+
+                    {errors.length > 0 && (
+                        <div>
+                            <ul className="ul-validation-error">
+                                {errors.map((err, index) => (
+                                    <li key={index}>{err}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+
                     <button type="submit" className="home-page">
                         Register
                     </button>
