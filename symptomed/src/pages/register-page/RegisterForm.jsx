c98f01d48f4c9732a6bf3d5c52022ba4bcc7f646
Georgina <georginamircheska@gmail.com>
2026-03-03 10:24:15 +0100

    Added styling and layout improvements for tablet displays

--- symptomed/src/pages/register-page/RegisterForm.jsx
+++ symptomed/src/pages/register-page/RegisterForm.jsx
@@ -59,12 +59,14 @@ export default function RegisterForm() {
             <form className="register-form" onSubmit={handleSubmit}>
                 <div className="register-input-wrapper">
                     <p className="register-p">Please fill your register data:</p>
+                    <div className="container-register-inputs">
                         <label htmlFor="register-email" className="register-email-label">Email:
                             <input type="email" id="register-email" value={email} className="register-email" onChange={(e) => {setEmail(e.target.value); setErrors([]); }} />
                         </label>
                         <label htmlFor="register-password" className="register-password-label">Password:
                             <input type="password" id="register-password" value={password} className="register-password" onChange={(e) => { setPassword(e.target.value); setErrors([]); }}/>
                         </label>
+                    </div>
                 </div>
 
                     {errors.length > 0 && (
